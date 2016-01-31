912c8e344d7b0654905cf988d111246efc45215c
David Scholberg <recombinant.vector@gmail.com>
2016-01-31 11:10:27 -0500

    register: moved module array outside of function

--- lib/modules/modpm/register.go
+++ lib/modules/modpm/register.go
@@ -4,15 +4,15 @@ import (
 	"github.com/davidscholberg/irkbot/lib"
 )
 
-func RegisterMods(registerMod func(m *lib.Module)) {
-	modules := []*lib.Module{
-		&lib.Module{nil, nil, Help},
-		&lib.Module{nil, nil, Url},
-		&lib.Module{ConfigEchoName, nil, EchoName},
-		&lib.Module{ConfigInsult, HelpInsult, Insult},
-		&lib.Module{nil, nil, Quit},
-		&lib.Module{nil, HelpUrban, Urban}}
+var modules []*lib.Module = []*lib.Module{
+	&lib.Module{nil, nil, Help},
+	&lib.Module{nil, nil, Url},
+	&lib.Module{ConfigEchoName, nil, EchoName},
+	&lib.Module{ConfigInsult, HelpInsult, Insult},
+	&lib.Module{nil, nil, Quit},
+	&lib.Module{nil, HelpUrban, Urban}}
 
+func RegisterMods(registerMod func(m *lib.Module)) {
 	for _, m := range modules {
 		if m.GetHelp != nil {
 			RegisterHelp(m.GetHelp())
