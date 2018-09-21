e4a32c22ff4f836b03fd86e407bb62b030dd1ed8
David Scholberg <recombinant.vector@gmail.com>
2018-09-21 00:07:27 -0400

    youtube: ran gofmt, fixed syntax error

--- lib/module/register.go
+++ lib/module/register.go
@@ -81,8 +81,8 @@ func RegisterModules(conn *irc.Connection, cfg *configure.Config, outChan chan m
 			cmdMap["doing"] = &CommandModule{ConfigDoing, HelpDoing, Doing}
 		case "doom":
 			cmdMap["doom"] = &CommandModule{nil, HelpDoom, Doom}
-                case "youtube":
-                        cmdMap["yt"] = &CommandModule{nil, HelpYoutube, Youtube}
+		case "youtube":
+			cmdMap["yt"] = &CommandModule{nil, HelpYoutube, Youtube}
 		default:
 			return fmt.Errorf("invalid name '%s' in module config", moduleName)
 		}
