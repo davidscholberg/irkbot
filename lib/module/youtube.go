70f14308aca160b419afe2fa6391eaa543628add
David Scholberg <recombinant.vector@gmail.com>
2018-12-01 23:59:19 -0500

    youtube: added playlist and channel result support

--- lib/module/youtube.go
+++ lib/module/youtube.go
@@ -37,25 +37,55 @@ func youtubeSearch(cfg *configure.Config, in *message.InboundMsg, actions *actio
 		actions.say("error creating youtube client")
 		return
 	}
-	call := service.Search.List("id,snippet").Q(msg).MaxResults(1).Type("video")
+	call := service.Search.List("id,snippet").Q(msg).MaxResults(1)
 	resp, err := call.Do()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error performing youtube search: %v\n", err)
 		actions.say("error performing youtube search")
 		return
 	}
-	var video = "no results found! ¯\\_(ツ)_/¯"
+	var result = "no results found! ¯\\_(ツ)_/¯"
 	for _, item := range resp.Items {
 		switch item.Id.Kind {
 		case "youtube#video":
-			video = fmt.Sprintf(
+			result = fmt.Sprintf(
 				"%s - https://www.youtube.com/watch?v=%s",
 				item.Snippet.Title,
 				item.Id.VideoId,
 			)
-			break
+		case "youtube#channel":
+			result = fmt.Sprintf(
+				"%s - https://www.youtube.com/channel/%s",
+				item.Snippet.Title,
+				item.Id.ChannelId,
+			)
+		case "youtube#playlist":
+			playlistId, err := getExternalPlaylistId(service, item.Id.PlaylistId)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error getting playlist id: %v\n", err)
+				break
+			}
+			result = fmt.Sprintf(
+				"%s - https://www.youtube.com/playlist?list=%s",
+				item.Snippet.Title,
+				playlistId,
+			)
 		}
 	}
 
-	actions.say(video)
+	actions.say(result)
+}
+
+// getExternalPlaylistId does an extra call to the youtube api to get the
+// actual external playlist ID that can be used in URLs.
+func getExternalPlaylistId(service *youtube.Service, playlistId string) (string, error) {
+	call := service.Playlists.List("id").Id(playlistId).MaxResults(1)
+	resp, err := call.Do()
+	if err != nil {
+		return "", err
+	}
+	for _, item := range resp.Items {
+		return item.Id, nil
+	}
+	return "", nil
 }
